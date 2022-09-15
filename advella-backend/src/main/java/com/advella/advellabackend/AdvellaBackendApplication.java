4c18145bdb204f275a7a54a49b26421c08aba084
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2022-09-15 13:25:29 +0200

    Project init

---
+++ advella-backend/src/main/java/com/advella/advellabackend/AdvellaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.advella.advellabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvellaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvellaBackendApplication.class, args);
+	}
+
+}
---
+++ advella-backend/src/test/java/com/advella/advellabackend/AdvellaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advella.advellabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvellaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
