c1343f463764e13876c0ae852aadf660ae891d81
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2022-11-01 15:56:35 +0100

    lowered permissions to get all categories

--- src/main/java/com/advella/advellabackend/controllers/ServiceCategoryController.java
+++ src/main/java/com/advella/advellabackend/controllers/ServiceCategoryController.java
@@ -15,7 +15,7 @@ public class ServiceCategoryController {
 
     private final ServiceCategoryService serviceCategoryService;
 
-    @GetMapping("/service-categories/dash-board/all")
+    @GetMapping("/service-categories/all")
     public ResponseEntity<List<ServiceCategory>> getServiceCategories() {
         return ResponseEntity.ok(serviceCategoryService.getServiceCategories());
     }
