18a07440a6b1a8c9326bf3875a8b2a883d3390f1
popalnicolas <56255509+popalnicolas@users.noreply.github.com>
2022-11-09 17:03:23 +0100

    Update SecurityConfig.java

--- src/main/java/com/advella/advellabackend/security/SecurityConfig.java
+++ src/main/java/com/advella/advellabackend/security/SecurityConfig.java
@@ -34,13 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-        customAuthenticationFilter.setFilterProcessesUrl("/api/users/login");
+        customAuthenticationFilter.setFilterProcessesUrl("/api/users/newlogin");
 
         http.cors();
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-        http.authorizeRequests().antMatchers("/api/users/login", "/api/users/register", "/swagger-ui/**, /content/**").permitAll();
+        http.authorizeRequests().antMatchers("/api/users/newlogin", "/api/users/register", "/swagger-ui/**, /content/**").permitAll();
         http.authorizeRequests().antMatchers("/api/productsAndServices/dash-board/**",
                 "/api/users/dash-board/**",
                 "/api/services/dash-board/**",
