984df16a376c4e2c0ce3bedbf2399ecd935af079
JanVasilcenko <62397372+JanVasilcenko@users.noreply.github.com>
2022-11-19 15:03:18 +0100

    test

--- src/main/java/com/advella/advellabackend/config/WebSocketConfig.java
+++ src/main/java/com/advella/advellabackend/config/WebSocketConfig.java
@@ -27,6 +27,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry
                 .addEndpoint("/ws")
+                .setAllowedOriginPatterns("http://localhost:3000/")
                 .withSockJS();
     }
 
